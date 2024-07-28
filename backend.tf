6d059e37993d957911c8b5cd51d740afb9e5949d
Kwasi <shev030391@gmail.com>
2024-07-28 17:52:16 -0400

    follow up

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "kb0303-terraform"
+    bucket         = "ljlljljiuumjk"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "locktable"
+    dynamodb_table = "jgjghjgjhjhj"
   }
 
 }
